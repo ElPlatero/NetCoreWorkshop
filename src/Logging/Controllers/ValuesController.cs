32854bed55059b49681c1292036777ad6c6fc8f5
Tino Schüttpelz <tino.schuettpelz@systemtechnik-online.de>
2018-10-19 12:20:26 +0200

    initial commit

---
+++ src/Logging/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Logging.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public ValuesController(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<ValuesController>();
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            using (_logger.BeginScope("Beispielscope"))
+            {
+                _logger.LogTrace("This is for development purposes only.");
+                _logger.LogDebug("This is for debugging only.");
+                _logger.LogInformation("This is an information.");
+                _logger.LogWarning("This is a warning.");
+                _logger.LogError("This is an error.");
+                _logger.LogCritical("This is critical.");
+
+                var result = new { Key = "value" };
+
+                _logger.LogInformation("Returning {@result}.", result);
+                return Ok(result);
+            }
+        }
+    }
+}
